5b015e45dcb15fdefa61d9063a575f20ae0974ae
xkoalu <y2f2r56nyc@privaterelay.appleid.com>
2026-08-31 14:37:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* GYM.LOG Service Worker
    Bei jeder neuen Version die Zahl in CACHE erhöhen –
    dadurch wird der alte Zwischenspeicher verworfen. */
-var CACHE = "gymlog-v3";
+var CACHE = "gymlog-v4";
 
 var SHELL = [
   "./",
